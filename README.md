c8b791aae46d9014f6b8ca08f1e20451086d1667
Daniel Morandini <danielmorandini@me.com>
2025-06-30 14:44:15 +0200

    Prepare for publication

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-# Membrane.NALU
-
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `membrane_nalu_plugin` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:membrane_nalu_plugin, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/membrane_nalu_plugin>.
+# Membrane NALU Plugin
+A parser/aggregator of NALU units for the membrane framework.
 
--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Membrane.NALU.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/kim-company/membrane_nalu_plugin",
+      name: "Membrane NALU Plugin",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -22,7 +26,23 @@ defmodule Membrane.NALU.MixProject do
   defp deps do
     [
       {:membrane_core, "~> 1.2"},
-      {:membrane_file_plugin, "~> 0.17", only: :test}
+      {:membrane_file_plugin, "~> 0.17", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    [
+      maintainers: ["KIM Keep In Mind"],
+      files: ~w(lib mix.exs README.md LICENSE),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/kim-company/membrane_nalu_plugin"}
+    ]
+  end
+
+  defp description do
+    """
+    NALU parser and aggregator for the Membrane Framework.
+    """
   end
 end
