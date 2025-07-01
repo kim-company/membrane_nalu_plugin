d44ecc243658deace73eadd7ea1cdf3da7124cd7
Daniel Morandini <danielmorandini@me.com>
2025-07-01 13:56:35 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Membrane.NALU.MixProject do
   def project do
     [
       app: :membrane_nalu_plugin,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
